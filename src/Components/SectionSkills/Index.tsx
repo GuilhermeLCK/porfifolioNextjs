a564bafa933bd52b52ec5045e62a3978d35b65d0
Guilherme Costa <guilhermelimacosta@gmail.com>
2023-05-18 02:47:42 -0300

    efeito AOS

---
+++ src/Components/SectionSkills/Index.tsx
@@ -0,0 +1,55 @@
+import sass from "../../Img/sass.svg";
+import figma from "../../Img/figma.svg";
+import git from "../../Img/git.svg";
+import js from "../../Img/js.svg";
+import react from "../../Img/reactjs.svg";
+import css from "../../Img/css.svg";
+import html from "../../Img/html.svg";
+import Image from "next/image";
+import styles from "./styles.module.scss";
+import { useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+export default function SkillMe() {
+  useEffect(() => {
+    Aos.init({
+      duration: 1500,
+    });
+  }, []);
+
+  return (
+    <>
+      <section>
+        <div className={styles.container} data-aos="zoom-out">
+          <div className={styles.containerH2}>
+            <h1>Habilidades</h1>
+          </div>
+          <div className={styles.containerItem}>
+            <div className={styles.item}>
+              <Image src={html} alt="html"></Image>
+            </div>
+            <div className={styles.item}>
+              <Image src={css} alt="css"></Image>
+            </div>
+            <div className={styles.item}>
+              <Image src={react} alt="react"></Image>
+            </div>
+            <div className={styles.item}>
+              <Image src={js} alt="js"></Image>
+            </div>
+            <div className={styles.item}>
+              <Image src={git} alt="git"></Image>
+            </div>
+            <div className={styles.item}>
+              <Image src={figma} alt="figma"></Image>
+            </div>
+            <div className={styles.item}>
+              <Image src={sass} alt="sass"></Image>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,27 @@
 import Header from "@/Components/Header/Index";
 import About from "@/Components/SectionAbout/Index";
 import IamMe from "@/Components/SectionIamMe/Index";
+import Skills from "@/Components/SectionSkills/Index";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    Aos.init({
+      duration: 1500,
+    });
+  }, []);
   return (
     <>
       <Header></Header>
-      <IamMe></IamMe>
-      <About></About>
+      <div data-aos="fade-up-left">
+        <IamMe></IamMe>
+      </div>
+      <div data-aos="fade-up-right">
+        <About></About>
+      </div>
+      <Skills></Skills>
       <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />{" "}
       <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />{" "}
       <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />{" "}
